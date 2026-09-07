fb7694ca70b63bb5e9a16e580e845184dee283e7
Naman <namantri007@gmail.com>
2026-09-07 19:15:10 +0530

    Prepare backend server for deployment

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const startServer = async () => {
   try {
     await connectDB();
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
   } catch (error) {
